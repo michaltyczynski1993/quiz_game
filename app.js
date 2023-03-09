012a2167f9a4e782deb25f92df67efbd8fb593e1
Michal Tyczynski <m.tyczynski1993@gmail.com>
2023-03-09 17:27:57 +0100

    basic quiz logic (one question)

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+import { questions } from "./questions.js";
+// selectors
+const questionText = document.getElementById('question');
+const buttons = document.getElementsByTagName('button');
+// pick a question object - just for test single record from Array
+let question = questions[0];
 // show question
+questionText.innerHTML = question.question;
 
-// selectors question desc; responses (4 options)
 
+// show buttons with replies from question
 // check correction
 
+for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.innerHTML = question.replies[i];
+    button.addEventListener('click', () => question.checkAnswer(button.innerHTML, button));
+}
 // move to next question
--- questions.js
+++ questions.js
@@ -1,4 +1,8 @@
-import { Question } from "./questionModel";
+import { Question } from "./questionModel.js";
 
-const questions = [
+export const questions = [
+    new Question('Jakie jest największe państwo na świecie pod względem powierzchni?',
+        ['Rosja', 'Kanada', 'Stany Zjednoczone', 'Chiny'], 'Rosja'),
+    new Question('Które miasto jest stolicą Włoch?',
+        { a: 'Rzym', b: 'Wenecja', c: 'Florencja', d: 'Neapol' }),
 ]
